12e7d7637ea8a2ccf2182c74fdc2f6255cd2566c
vojtasmrcek <planarvoid@gmail.com>
2020-01-07 17:32:02 +0100

    Code test update

--- FreshlyPressed/src/main/java/com/automattic/freshlypressed/PostListFragment.java
+++ FreshlyPressed/src/main/java/com/automattic/freshlypressed/PostListFragment.java
@@ -2,7 +2,9 @@ package com.automattic.freshlypressed;
 
 import android.app.ListFragment;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -14,7 +16,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.function.Consumer;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -22,8 +28,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class PostListFragment extends ListFragment implements AdapterView.OnItemClickListener {
-    OkHttpClient mClient = new OkHttpClient();
-
+    PostsApi postsApi = new PostsApi(new OkHttpClient());
     // empty constructor necessary for fragments
     public PostListFragment() {
     }
@@ -55,15 +60,7 @@ public class PostListFragment extends ListFragment implements AdapterView.OnItem
     }
 
     public void refreshPosts() {
-        Request request = new Request.Builder()
-                .url("https://public-api.wordpress.com/rest/v1/freshly-pressed?number=30")
-                .build();
-
-        Response response = mClient.newCall(request).execute();
-
-        JSONObject json = new JSONObject(response.body().string());
-        JSONArray postsJson = json.getJSONArray("posts");
-        updatePosts(postsJson);
+        updatePosts(postsApi.loadPosts());
     }
 
     private class PostsAdapter extends BaseAdapter {
@@ -84,16 +81,43 @@ public class PostListFragment extends ListFragment implements AdapterView.OnItem
 
         @Override
         public View getView(int position, View view, ViewGroup parent) {
-            View itemView = getActivity().getLayoutInflater()
-                    .inflate(R.layout.post_list_fragment_item, parent, false);
+            Post previousItem = position > 0 ? getItem(position - 1) : null;
+            Post post = getItem(position);
+            boolean isNewDay = false;
+            try {
+                isNewDay = previousItem == null || (previousItem.getDate().getTime() - 60000 * 60 * 24) > post.getDate().getTime();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            final View itemView = getActivity().getLayoutInflater()
+                    .inflate(!isNewDay ? R.layout.post_list_fragment_item : R.layout.post_list_fragment_header_item, parent, false);
+
+            if (isNewDay) {
+                TextView header = itemView.findViewById(R.id.header);
+                try {
+                    header.setText(PostUtils.INSTANCE.printDate(post.getDate()));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
 
             TextView title = itemView.findViewById(R.id.title);
             TextView summary = itemView.findViewById(R.id.summary);
+            TextView author = itemView.findViewById(R.id.author_name);
 
-            Post post = getItem(position);
 
             title.setText(post.getTitle());
             summary.setText(Html.fromHtml(post.getExcerpt().toString()));
+            author.setText(post.getAuthor());
+
+            new LoadCountTask(new LoadCountCallback() {
+                @Override
+                public void run(int param) {
+                    TextView postCount = itemView.findViewById(R.id.subscribers_count);
+                    postCount.setText(String.valueOf(param));
+                }
+            }).execute(post.getAuthorUrl().getHost());
 
             return itemView;
         }
@@ -107,5 +131,29 @@ public class PostListFragment extends ListFragment implements AdapterView.OnItem
         public int getCount() {
             return mPostData.length();
         }
+    }
+
+    private class LoadCountTask extends AsyncTask<String, Void, Integer> {
+
+        private LoadCountCallback callback;
+
+        LoadCountTask(LoadCountCallback callback) {
+            this.callback = callback;
+        }
+
+        @Override
+        protected Integer doInBackground(String... strings) {
+            String blogUrl = strings[0];
+            return postsApi.loadSubscribersCount(blogUrl);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            callback.run(integer);
+        }
+    }
+
+    interface LoadCountCallback {
+        void run(int param);
     }
 }
