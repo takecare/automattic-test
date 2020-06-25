e6544b046b7795e6f2215cb3ed5309ba92327a37
takecare <rui@vazteixeira.org>
2020-06-25 09:09:47 +0100

    add factory method to PostsService

--- FreshlyPressed/src/main/java/com/automattic/freshlypressed/data/PostsService.kt
+++ FreshlyPressed/src/main/java/com/automattic/freshlypressed/data/PostsService.kt
@@ -1,10 +1,25 @@
 package com.automattic.freshlypressed.data
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// https://public-api.wordpress.com/rest/v1.1/sites/discover.wordpress.com/posts?number=1&meta=site&fields=author,date,URL,title,excerpt,featured_image,meta
+
 interface PostsService {
 
-    @GET("discover.wordpress.com/posts")
+    @GET("discover.wordpress.com/posts?number=1&meta=site&fields=author,date,URL,title,excerpt,featured_image,meta")
     suspend fun getPosts(@Query("number") number: Int = 10): Posts
+
+    companion object {
+        fun createService(okHttpClient: OkHttpClient) =
+            Retrofit.Builder()
+                .baseUrl("https://public-api.wordpress.com/rest/v1.1/sites/")
+                .addConverterFactory(MoshiConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+                .create(PostsService::class.java)
+    }
 }
